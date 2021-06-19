45e1a930c356b5a2d1d4d88216525d9d65f558f4
xiraix <xiraix@protonmail.com>
2021-06-19 05:55:53 -0400

    basic mem rw

--- arch/x86/kvm/cheatcall.c
+++ arch/x86/kvm/cheatcall.c
@@ -7,14 +7,15 @@
 
 
 enum CC_COMMAND_ID{
-	PRINT_PHYSICAL_QWORD,
-	PRINT_VIRTUAL_QWORD,
+	PHYSICAL_READ,
+	PHYSICAL_WRITE,
 };
 
 struct CC_COMMAND {
 	enum CC_COMMAND_ID id;
 	unsigned long from;
 	unsigned long to;
+	unsigned long size;
 };
 
 void dump_cmd(struct CC_COMMAND* cmd){
@@ -26,6 +27,82 @@ void dump_cmd(struct CC_COMMAND* cmd){
 		"--------\n\n",
 		cmd->id, cmd->from, cmd->to
 	);
+}
+
+//from is gva
+//to is pgpa
+void do_physical_read(struct kvm_vcpu* vcpu, struct CC_COMMAND* cmd)
+{
+	//TODO: USE PROPER MEMORY ALLCATION
+	char* buffer[0x20];
+	struct x86_exception exception = {0};
+	int ret;
+
+	unsigned long from_pa = cmd->from;
+	unsigned long to_va = cmd->to;
+	unsigned long size = cmd->size;
+
+	if(
+		(from_pa && to_va && size) == 0
+	){
+		pr_err("do_physcal_read: FAILED - BAD cmd");
+		goto out;
+	}
+
+	ret = kvm_vcpu_read_guest(vcpu, from_pa, buffer, size);
+	if(ret != 0){
+		pr_err("do_physcal_read: FAILED - kvm_vcpu_read_guest failed with %u", ret);
+		goto out;
+	}
+
+	kvm_write_guest_virt_system(vcpu, to_va, buffer, size, &exception);
+	if(ret !=  X86EMUL_CONTINUE){
+		pr_err("do_physcal_read: FAILED - kvm_write_guest_virt_system failed with %u", ret);
+		goto out;
+	}
+
+out:
+	return;
+
+}
+
+
+//from is gva
+//to is pgpa
+void do_physical_write(struct kvm_vcpu* vcpu, struct CC_COMMAND* cmd)
+{
+	//TODO: USE PROPER MEMORY ALLCATION
+	char* buffer[0x20];
+	struct x86_exception exception = {0};
+	int ret;
+
+	unsigned long from_va = cmd->from;
+	unsigned long to_pa = cmd->to;
+	unsigned long size = cmd->size;
+
+	if(
+		(from_va && to_pa && size) == 0
+	){
+		pr_err("do_physical_write: FAILED - BAD cmd");
+		goto out;
+	}
+
+	ret = kvm_read_guest_virt(vcpu, from_va, buffer, size, &exception);
+	if(ret != X86EMUL_CONTINUE){
+		pr_err("do_physical_write: FAILED - Exception code: %u", exception.error_code);
+		goto out;
+	}
+
+	ret = kvm_vcpu_write_guest(vcpu, to_pa, buffer, size);
+	if(ret != 0){
+		pr_err("do_physcal_read: FAILED - kvm_vcpu_write_guest failed with %u", ret);
+		goto out;
+	}
+
+
+out:
+	return;
+
 }
 
 int cheatcall_do(struct kvm_vcpu* vcpu, unsigned long a0, unsigned long a1, unsigned long a2, unsigned long a3){
@@ -54,6 +131,12 @@ int cheatcall_do(struct kvm_vcpu* vcpu, unsigned long a0, unsigned long a1, unsi
 	dump_cmd(&cmd);
 
 	switch(cmd.id){
+		case PHYSICAL_READ:
+			do_physical_read(vcpu, &cmd);
+			break;
+		case PHYSICAL_WRITE:
+			do_physical_write(vcpu, &cmd);
+			break;
 		default:
 			break;
 	}
